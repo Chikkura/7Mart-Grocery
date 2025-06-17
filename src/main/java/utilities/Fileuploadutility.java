0536f69e5fe371a8238aa04b5cfb467a1354d8bc
Chikkura <chikku@revyrieglobal.com>
2025-06-17 15:00:31 +0530

    Initial Commit

--- src/main/java/utilities/Fileuploadutility.java
+++ src/main/java/utilities/Fileuploadutility.java
@@ -1,5 +1,11 @@
 package utilities;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -10,7 +16,17 @@ public class Fileuploadutility {
 		element.sendKeys(path);
 		
 	}
-public void roboclassforfileupload(WebElement element, String path) {
+public void roboclassforfileupload(WebElement element, String path) throws AWTException {
+	StringSelection selection=new StringSelection(path);
+	Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection,null);
+	Robot robot=new Robot();
+	robot.delay(2000);
+	robot.keyPress(KeyEvent.VK_CONTROL);
+	robot.keyPress(KeyEvent.VK_V);
+	robot.keyRelease(KeyEvent.VK_CONTROL);
+	robot.keyRelease(KeyEvent.VK_V);
+	robot.keyPress(KeyEvent.VK_ENTER);
+	robot.keyRelease(KeyEvent.VK_ENTER);
 	
 	
 }
